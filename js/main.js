d9a1f36270da49cb21bc787cdac0b76e88b5ecb3
zaha1223 <muhammadzohidbahodirov04@gmail.com>
2022-11-10 18:19:57 +0500

    Start

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var elBtn = document.querySelector(".header__menu");
+var elModal = document.querySelector(".modal");
+var elDel = document.querySelector(".modal__del");
+let elBody = document.querySelector(".site-body");
+
+elBtn.addEventListener("click", function () {
+  elModal.classList.toggle("open");
+});
+elDel.addEventListener("click", function () {
+  elModal.classList.remove("open");
+});
+let elBtnCL = document.querySelector(".hero__link");
+
+elBtnCL.addEventListener("click", function () {
+  elBody.classList.toggle("st-body");
+});
